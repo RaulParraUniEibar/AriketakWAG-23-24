f6747fa7ca66d92d790967f36aab3134761fedf7
RaulParraUniEibar <75114165+RaulParraUniEibar@users.noreply.github.com>
2023-09-20 11:48:37 +0200

    20/09/2023

--- KamisetakAriketa/scriptak.js
+++ KamisetakAriketa/scriptak.js
@@ -1,25 +1,29 @@
-function aldatuAtzera(){
-
+function aldatuAtzera() {
     var imagen = document.getElementById("1");
+    var imagenSrc = imagen.getAttribute("src");
 
-    if (imagen.src === "http://127.0.0.1:5500/img/6.jpg") {
+    if (imagenSrc === "img/6.jpg") {
         alert("Ezin da atzera egin");
-    } else if (imagen.src === "http://127.0.0.1:5500/img/9.jpg") {
-        imagen.src = "http://127.0.0.1:5500/img/6.jpg";
-    } else if (imagen.src === "http://127.0.0.1:5500/img/11.jpg") {
-        imagen.src = "http://127.0.0.1:5500/img/9.jpg";
+    } else if (imagenSrc === "img/9.jpg") {
+        imagen.setAttribute("src", "img/6.jpg");
+        document.getElementById("numcami").textContent = "Camiseta 6"; // Cambia el texto a "Camiseta 6"
+    } else if (imagenSrc === "img/11.jpg") {
+        imagen.setAttribute("src", "img/9.jpg");
+        document.getElementById("numcami").textContent = "Camiseta 9"; // Cambia el texto a "Camiseta 9"
     }
 }
 
-function aldatuAurrera(){
-    
+function aldatuAurrera() {
     var imagen = document.getElementById("1");
+    var imagenSrc = imagen.getAttribute("src");
 
-    if (imagen.src === "http://127.0.0.1:5500/img/6.jpg") {
-        imagen.src = "http://127.0.0.1:5500/img/9.jpg";
-    } else if (imagen.src === "http://127.0.0.1:5500/img/9.jpg") {
-        imagen.src = "http://127.0.0.1:5500/img/11.jpg";
-    } else if (imagen.src === "http://127.0.0.1:5500/img/11.jpg") {
+    if (imagenSrc === "img/6.jpg") {
+        imagen.setAttribute("src", "img/9.jpg"); // te cambia el src que es img/6.jpg en el index y te lo cambia al 9
+        document.getElementById("numcami").textContent = "Camiseta 9"; // Cambia el texto a "Camiseta 9"
+    } else if (imagenSrc === "img/9.jpg") {
+        imagen.setAttribute("src", "img/11.jpg");
+        document.getElementById("numcami").textContent = "Camiseta 11"; // Cambia el texto a "Camiseta 11"
+    } else if (imagenSrc === "img/11.jpg") {
         alert("Ezin da aurrera egin");
     }
 }
