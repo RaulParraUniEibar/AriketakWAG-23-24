90bac52dadc2eeee0d08e973a509956d845cf546
RaulParraUniEibar <75114165+RaulParraUniEibar@users.noreply.github.com>
2023-09-20 12:01:52 +0200

    KamisetakAldatuV2

---
+++ KamisetakAriketa2/Mugimendua.js
@@ -0,0 +1,30 @@
+var imagenes = ["img/6.jpg", "img/9.jpg", "img/11.jpg"];
+var indice = 0;
+var imagen = document.getElementById("camiImg");
+var nombreArchivoElement = document.getElementById("nombreArchivo");
+
+function aldatuAtzera() {
+    if (indice > 0) {
+        indice--;
+        imagen.src = imagenes[indice];
+        KamisetaIzenaAldatu();
+    } else {
+        alert("Ezin da atzera egin");
+    }
+}
+
+function aldatuAurrera() {
+    if (indice < imagenes.length - 1) {
+        indice++;
+        imagen.src = imagenes[indice];
+        KamisetaIzenaAldatu();
+    } else {
+        alert("Ezin da aurrera egin");
+    }
+}
+
+function KamisetaIzenaAldatu() {
+    var nombreImagen = imagenes[indice];
+    var nombreSinExtension = nombreImagen.split("/").pop().split(".")[0];
+    nombreArchivoElement.textContent = nombreSinExtension;
+}
