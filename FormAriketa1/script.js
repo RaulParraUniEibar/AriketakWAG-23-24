f8845363c5c566bb50e80ac7aedfaa5acabf006d
RaulParraUniEibar <75114165+RaulParraUniEibar@users.noreply.github.com>
2023-09-22 12:17:13 +0200

    22/09/2023
    Pedro eta Belma ariketa amaituta

--- FormAriketa1/script.js
+++ FormAriketa1/script.js
@@ -10,3 +10,25 @@ function Cambiarfoto() {
         imagenGenero.src = "img/caraMujer.jpg";
     }
 }
+
+function mostrarDatos() {
+    // Obtener los valores de los campos del formulario
+    var genre = document.querySelector('input[name="gender"]:checked').value;
+    var izena = document.getElementById("izena").value;
+    var abizena = document.getElementById("abizena").value;
+    var helbidea = document.getElementById("helbidea").value;
+    var hiria = document.getElementById("hiria").value;
+    var gustokoena = document.querySelector('input[name="Gustokoena"]:checked').value;
+
+    // Construir un mensaje con los datos
+    var mensaje = "<b> Zuzentzeko era: </b>" + genre + "<br>";
+    mensaje += "<b> Izena: </b>" + izena + "<br>";
+    mensaje += "<b> Abizena: </b>" + abizena + "<br>";
+    mensaje += "<b> Helbidea: </b>" + helbidea + "<br>";
+    mensaje += "<b> Hiria: </b>" + hiria + "<br>";
+    mensaje += "<b> Gustokoena: </b>" + gustokoena + "<br>";
+    
+    
+    // Mostrar el mensaje en el div resultado
+    document.getElementById("datosformulario").innerHTML =  mensaje;
+}
