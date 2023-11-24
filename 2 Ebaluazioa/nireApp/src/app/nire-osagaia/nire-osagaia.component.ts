2659cb2e9a2fc38b1c1f82cc1c42487b529edd3b
RaulParraUniEibar <75114165+RaulParraUniEibar@users.noreply.github.com>
2023-11-24 12:11:34 +0100

    Angular 1 ariketak Amaituta
    24/11/2023
    12:11

--- 2 Ebaluazioa/nireApp/src/app/nire-osagaia/nire-osagaia.component.ts
+++ 2 Ebaluazioa/nireApp/src/app/nire-osagaia/nire-osagaia.component.ts
@@ -9,6 +9,8 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './nire-osagaia.component.html',
   styleUrl: './nire-osagaia.component.css'
 })
+
+
 export class NireOsagaiaComponent {
   /*langilea = {
     izena: 'Pepe',
@@ -17,18 +19,34 @@ export class NireOsagaiaComponent {
     irudia: './assets/img/Vilma.jpg'
     };
 */
+  myText = "Nire izena: Raúl da";
+
   Title="Langileen zerrenda";
+  rowStart = 0;
+  rowLimit = 1;
+    kenduRow() {
+    this.rowStart -= 1;
+    this.rowLimit -= 1;
+    }
+    gehituRow() {
+    this.rowStart += 1;
+    this.rowLimit += 1;
+    }
+
     getTitle() {
     return this.Title;
     };
+    
 
     langileak = [
-      { izena: "Pepe", abizena: "Perez", generoa: "gizona", soldata: 1000,likes: 0,lanean: true },
-      { izena: "Sara", abizena: "Ruiz", generoa: "emakumea", soldata: 1000,likes: 0,lanean: true },
-      { izena: "Paul", abizena: "Elorza", generoa: "gizona", soldata: 1500,likes: 0,lanean: false },
-      { izena: "Raul", abizena: "Txakartegi", generoa: "gizona", soldata: 2000,likes: 0,lanean: false }
+      { izena: "Pepe", abizena: "Perez", generoa: "gizona", soldata: 1000,likes: 0,lanean: true,jaiotza: new Date(3/3/2023)},
+      { izena: "Sara", abizena: "Ruiz", generoa: "emakumea", soldata: 1000,likes: 0,lanean: true,jaiotza: new Date(1/1/2023)},
+      { izena: "Paul", abizena: "Elorza", generoa: "gizona", soldata: 1500,likes: 0,lanean: false,jaiotza: new Date(2/7/2023) },
+      { izena: "Raul", abizena: "Txakartegi", generoa: "gizona", soldata: 2000,likes: 0,lanean: false,jaiotza: new Date(25/12/2023) }
       ];
 
     gehituLikes(langilea: any)
     { langilea.likes++; }
+
+    
 }
