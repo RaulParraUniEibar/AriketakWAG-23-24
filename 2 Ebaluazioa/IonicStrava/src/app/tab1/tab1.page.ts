f5bf0f658e7cedb63d6a1463a2299740d14879ea
RaulParraUniEibar <75114165+RaulParraUniEibar@users.noreply.github.com>
2023-11-29 13:24:09 +0100

    Ariketa 3 RestApi barik
    29/11/2023
    13:24

--- 2 Ebaluazioa/IonicStrava/src/app/tab1/tab1.page.ts
+++ 2 Ebaluazioa/IonicStrava/src/app/tab1/tab1.page.ts
@@ -9,4 +9,15 @@ export class Tab1Page {
 
   constructor() {}
 
+  kluba = {
+    id: 783189,
+    name: 'uni eibar',
+    cover_photo_small: 'assets/img/kluba.jpg',
+    sport_type: 'running',
+    private: true,
+    member_count: 3,
+    description: 'Lanbide Heziketako Ikastetxea',
+    club_type: 'company',
+  };
+
 }
