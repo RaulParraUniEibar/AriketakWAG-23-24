f828dc7d9e4807b94c1d24a6f939f0e4d3595f63
RaulParraUniEibar <75114165+RaulParraUniEibar@users.noreply.github.com>
2024-01-16 11:00:49 +0100

    jarduera-gehitu
    jarduera-gehitu modulua

---
+++ 2 Ebaluazioa/IonicStrava_Markel/src/app/jarduera-gehitu/jarduera-gehitu.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jarduera-gehitu',
+  templateUrl: './jarduera-gehitu.page.html',
+  styleUrls: ['./jarduera-gehitu.page.scss'],
+})
+export class JardueraGehituPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
